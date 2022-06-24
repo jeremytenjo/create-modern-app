196c216fa7902e47356826f2b5d8b3b1aea1ff10
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-06-24 13:51:32 -0400

    added tests

---
+++ tests/unit.tests/create.app.tests.ts
@@ -0,0 +1,15 @@
+import { test } from 'vitest'
+
+import shell from '../../src/utils/shell/shell.js'
+
+test('Create webapp', async () => {
+  await shell('npm run test:create-webapp-dev')
+}, 40000)
+
+test('Create website', async () => {
+  await shell('npm run test:create-website-dev')
+}, 40000)
+
+test('Create demo', async () => {
+  await shell('npm run test:create-demo-dev')
+}, 40000)
