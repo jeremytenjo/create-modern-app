769cf16375734b89c99706abc9dc3cc68a6f4ce7
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-06-24 14:07:27 -0400

    run tests concurrently

--- tests/unit.tests/create.app.tests.ts
+++ tests/unit.tests/create.app.tests.ts
@@ -1,16 +1,30 @@
 // https://vitest.dev/api/
-import { test } from 'vitest'
+import { describe, it } from 'vitest'
 
 import shell from '../../src/utils/shell/shell.js'
 
-test('Create webapp', async () => {
-  await shell('npm run test:create-webapp-dev')
-}, 40000)
+describe('Create app', () => {
+  it.concurrent(
+    'Create webapp',
+    async () => {
+      await shell('npm run test:create-webapp-dev')
+    },
+    40000,
+  )
 
-test('Create website', async () => {
-  await shell('npm run test:create-website-dev')
-}, 40000)
+  it.concurrent(
+    'Create website',
+    async () => {
+      await shell('npm run test:create-website-dev')
+    },
+    40000,
+  )
 
-test('Create demo', async () => {
-  await shell('npm run test:create-demo-dev')
-}, 40000)
+  it.concurrent(
+    'Create demo',
+    async () => {
+      await shell('npm run test:create-demo-dev')
+    },
+    40000,
+  )
+})
